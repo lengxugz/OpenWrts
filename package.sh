947aa7c71d13e72519da77f453ffd2fa9ce439d2
lengxugz <lengxugz@users.noreply.github.com>
2026-06-30 14:07:11 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
+#git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
+#git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
 # Mentohust
 git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
